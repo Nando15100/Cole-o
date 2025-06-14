ec5cca5cbdf28ceb9731d6676d5e21aca4173e3c
Nando15100 <fernando.202111208@unilasalle.edu.br>
2025-06-14 17:58:03 -0300

    Create NOME_DA_COLECAO_PRIMEIRA_LETRA_MAIUSCULAS.js
    Criar coleção

---
+++ NOME_DA_COLECAO_PRIMEIRA_LETRA_MAIUSCULAS.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const NOME_DA_COLECAO_PRIMEIRA_LETRA_MAIUSCULASchema = new mongoose.Schema({
+  customId: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  nomeInput1: {
+    type: String,
+    required: true
+  },
+  nomeInput2: {
+    type: Number,
+    required: true
+  },
+  nomeInput3: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true // Isso adiciona createdAt e updatedAt automaticamente
+});
+
+module.exports = mongoose.model('NOME_DA_COLECAO_PRIMEIRA_LETRA_MAIUSCULA', NOME_DA_COLECAO_PRIMEIRA_LETRA_MINUSCULASchema);
+//POR EXEMPLO, SE O NOME FOR 'Medida' SERÁ CONVERTIDO PARA medidas (MINÚSCULO E NO PLURAL) COMO NOME DA COLEÇÃO.
